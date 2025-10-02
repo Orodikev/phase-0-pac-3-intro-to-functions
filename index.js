7a5272b85fe55599b60b67633824c17812ed6002
Orodikev <kevinyengo@gmail.com>
2025-10-02 07:43:40 +0300

    functions

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Follow along with the examples here
+function sayHello(firstName) {
+  console.log(`Hello, ${firstName}!`);
+}
+
+sayHello("Orodi");
+sayHello("Ayo");
+sayHello("Sade");
+sayHello("Tunde");
+sayHello("Bola");
+sayHello("Chidi");
+sayHello("Ngozi");
+
+function diff(y, x) {
+  return y - x;
+}
+console.log(diff(8, 10)); 
